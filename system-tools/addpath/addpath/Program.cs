ddf507964f0b72fcf6c913dd268464eede376a62
Lyxica <hello@lyxi.ca>
2020-12-02 08:58:15 -0800

    Add addpath tool

---
+++ system-tools/addpath/addpath/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+namespace addpath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine);
+            string NEW_PATH;
+            if (path[path.Length - 1] == ';')
+            {
+                NEW_PATH = String.Format("{0}{1}", path, Environment.CurrentDirectory);
+            }
+            else
+            {
+                NEW_PATH = String.Format("{0};{1}", path, Environment.CurrentDirectory);
+            }
+
+            Environment.SetEnvironmentVariable("PATH", NEW_PATH, EnvironmentVariableTarget.Machine);
+            Console.WriteLine(NEW_PATH);
+        }
+    }
+}
